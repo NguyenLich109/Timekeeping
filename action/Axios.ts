2a31e772c437deaa372361d5e4edfa071b62f09c
NguyenLich109 <nguyenvanlich199@gmail.com>
2023-08-16 21:55:36 +0700

    doi port

--- action/Axios.ts
+++ action/Axios.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const http = axios.create({
-  baseURL: "http://localhost:5000/api",
+  // baseURL: "http://localhost:5000/api",
+  baseURL: "https://api-timekeeping.vercel.app/api",
 });
 
 export default http;
